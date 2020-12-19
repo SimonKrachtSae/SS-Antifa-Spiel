a4bdd299b1ba5023c58f599947a7f84c77cd03d8
Simon Kracht <simon123kracht@gmail.com>
2020-12-19 18:08:39 +0100

    Initialize Projekt

---
+++ MiniGame SS/MiniGame SS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniGame_SS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
